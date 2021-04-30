3deaeb450fced1c6aef688e4835045c7864f5d11
CesarRawr <rawrcesar13@gmail.com>
2021-04-30 10:47:14 -0500

    rwr

--- public/js/App.js
+++ public/js/App.js
@@ -14,7 +14,7 @@ const main = () => {
 
 	const accessToken = 'pk.eyJ1IjoiY2VzYXItcndyIiwiYSI6ImNrbnYxM2w5NDA3OTMyd3M1eDNmNmExdGoifQ.Ct4E5u0Xkq9FWo2SASOszg';
 
-	let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	let mymap = L.map('mapid').setView([19.532710, -96.927801], 13);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
